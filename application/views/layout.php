88c61af19d2b3128780da3c75f2c279130e5c427
csanchez19 <csanchezf197@gmail.com>
2021-05-17 12:28:54 +0200

    hamburgeur

--- application/views/layout.php
+++ application/views/layout.php
@@ -7,8 +7,8 @@
     <!--CSS-->
     <link rel="stylesheet" type="text/css" href="https://cdn.tutorialjinni.com/jquery-bracket/0.11.1/jquery.bracket.min.css" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel = "stylesheet" type = "text/css" href = "<?php echo base_url(); ?>css/custom.css">
-    <link rel="stylesheet" href="<?php echo base_url(); ?>hamburger/style.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>css/custom.css">
+    <!--<link rel = "stylesheet" type = "text/css" href = "<?php echo base_url(); ?>css/custom.css">-->
     <script src="<?php echo base_url(); ?>hamburger/script.js"></script>
     <!--SCRIPTS-->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
@@ -20,6 +20,7 @@
     <title><?= $title ?></title>
 </head>
 <body>
+
 
 <div  class="open">
 	<span class="cls"></span>
