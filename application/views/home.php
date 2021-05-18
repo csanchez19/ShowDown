6578ed6c5c22264ff9faebda9ee4addb9a4590ef
csanchez19 <csanchezf197@gmail.com>
2021-05-18 10:32:11 +0200

    yeyes

--- application/views/home.php
+++ application/views/home.php
@@ -1,9 +1,9 @@
 
 <div class="container-fluid top_div">
     <div class="row">
-        <div class="col-5 offset-6 div_botones">
-            <h1 class="text-center"><?= $title ?></h1>
-            <div class="row">
+        <div class="col-lg-5 offset-lg-6 col-12 div_botones">
+            <h1 class="text-center">ShowDown! <br> Competeix i Guanya!</h1>
+            <div class="row mt-5">
                 <div class="col-6 text-center">
                     <button class="btnLogin">LOGUEJAR-SE</button>
                 </div>
