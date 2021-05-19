82ee8ee3f8dad6bfd5b3d8bdbe2b6299c5a3da03
csanchez19 <csanchezf197@gmail.com>
2021-05-19 09:45:42 +0200

    dadsa

---
+++ application/views/tournaments.php
@@ -0,0 +1,79 @@
+<div class="container">
+    <div class="main_tournament">
+        <div class="row d-block">
+            <div class="col-12 first_half"></div>
+        </div>
+        <div class="row">
+            <div class="col-12 main_tournament_text">
+                <p class="text-left">Torneig increible</p>
+                <div class="row">
+                    <div class="col-6 text-left">
+                        <p>21 places - MAIG 13, 15:00 EST</p>
+                    </div>
+                    <div class="col-6 text-right">
+                        <p>Hello Kitty Online</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container mt-5">
+    <div class="row">
+        <div class="col-4 torneo1 torneo">
+            <div class="row">
+                <div class="col-12 first_half"></div>
+            </div>
+            <div class="row">
+                <div class="col-12 tournament-text">
+                    <p class="text-left">Copa Demacia 5vs5</p>
+                    <div class="row">
+                        <div class="col-6">
+                            <p class="text-left">8 Places MAIG 14, 18:00 EST</p>
+                        </div>
+                        <div class="col-6">
+                            <p class="text-right">League Of Legends</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-4 torneo2 torneo">
+            <div class="row">
+                    <div class="col-12 first_half"></div>
+                </div>
+                <div class="row">
+                    <div class="col-12 tournament-text">
+                    <p class="text-left">Street Fighter 1vs1</p>
+                        <div class="row">
+                            <div class="col-6">
+                                <p class="text-left">8 Places MAIG 14, 18:00 EST</p>
+                            </div>
+                            <div class="col-6">
+                                <p class="text-right">Street Fighter</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        <div class="col-4 torneo3 torneo">
+            <div class="row">
+                    <div class="col-12 first_half"></div>
+                </div>
+                <div class="row">
+                    <div class="col-12 tournament-text">
+                    <p class="text-left">Furbo 2vs2</p>
+                        <div class="row">
+                            <div class="col-6">
+                                <p class="text-left">8 Places MAIG 14, 18:00 EST</p>
+                            </div>
+                            <div class="col-6">
+                                <p class="text-right">FIFA</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </div>
+</div>
