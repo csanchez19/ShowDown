110621dfb4cdc5777c04e496ab42e785dc7df2dd
csanchez19 <csanchezf197@gmail.com>
2021-05-21 09:16:06 +0200

    La Winners League és un esdeveniment temàtic trimestral que consisteix en un sistema d’acumulació de punts que s’obtenen al llarg del trimestre segons els resultats obtinguts en els diferents tornejos als que participa l’usuari. Quan queda una setmana de trimestre, comença el període de bescanviament, o el període de Points2Prizes, durant aquest temps, els usuaris podran, amb la quantitat de punts que hagin obtingut, aconseguir premis que oferiran els administradors. Gràcies a la situació actual epidemiològica, al moviment revolucionari que hi ha actualment envers al mon dels videojocs, i també, amb l’ajuda de la idea de la Winners League, creiem que és el moment més òptim per a llançar aquesta proposta.

---
+++ application/views/perfil.php
@@ -0,0 +1,12 @@
+<link rel="stylesheet" href="<?php echo base_url(); ?>css/register.css">
+<div class="container-fluid top_div">
+    <div class="row">
+        <div class="text-center col-12 div_botones">
+            
+        </div>
+    </div>
+</div>
+
+<div class="container">
+    
+</div>
