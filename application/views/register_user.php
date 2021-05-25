c746427c5c74cbb4c164ef917d6c66312cc1ac10
Andres <anlon636@gmail.com>
2021-05-25 09:43:11 +0200

    conexion register_user con controlador

--- application/views/register_user.php
+++ application/views/register_user.php
@@ -1,4 +1,8 @@
-<link rel="stylesheet" href="<?php echo base_url(); ?>css/register.css">
+<link rel="stylesheet" href="<?php
+
+use function PHPSTORM_META\type;
+
+echo base_url(); ?>css/register.css">
 <div class="container-fluid top_div">
     <div class="row">
         <div class="text-center col-12 div_botones">
@@ -9,55 +13,140 @@
 
 <div class="container form">
         <div class="marco offset-2 col-xl-8">
-            <form action="">
+
+            <?php echo form_open('showdown/validation'); ?>
+
                 <div class="pt-1  text-center divCobrament">
                     <p class="dadesCobrament">Dades Personals</p>
                 </div>
                 <div class="form-group mt-5 pt-5">
                     <div class="row">
                         <div class="col">
-                            <label for="Nom" class="sr-only">Nom</label>
-                            <input type="text" class="form-control" placeholder="Nom">
+                            <label for="nom" class="sr-only">Nom</label>
+                            <?php 
+                            $valueNom=(!empty($nom))?$nom:'';
+                            $dataNom = array(
+                                'name' => 'nom',
+                                'value' => $valueNom,
+                                'class' => 'form-control',
+                                'placeholder' => 'Nom'
+                            );
+                            echo form_input($dataNom);
+                            echo form_error('nom'); ?>
                         </div>
                         <div class="col">
-                            <label for="Cognom" class="sr-only">Cognom</label>
-                            <input type="text" class="form-control" placeholder="Cognoms">
-                        </div>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <div class="row">
-                        <div class="col">
-                            <label for="Username" class="sr-only">Username</label>
-                            <input type="text" class="form-control" placeholder="Nom d'Usuari">
-                        </div>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <div class="row">
-                        <div class="col">
-                            <label for="DNI" class="sr-only">DNI</label>
-                            <input type="text" class="form-control" placeholder="DNI">
-                        </div>
-                        <div class="col">
-                            <label for="Data de Naixement" class="sr-only">Data de Naixement</label>
-                            <input type="date" class="form-control" placeholder="">
+                            <label for="cognoms" class="sr-only">Cognom</label>
+                            <?php 
+                            $valueCognoms=(!empty($cognoms))?$cognoms:'';
+                            $dataCognoms = array(
+                                'name' => 'cognoms',
+                                'value' => $valueCognoms,
+                                'class' => 'form-control',
+                                'placeholder' => 'Cognoms'
+                            );
+                            echo form_input($dataCognoms);
+                            echo form_error('cognoms'); ?>
                         </div>
                     </div>
                 </div>
                 <div class="form-group">
                     <div class="row">
                         <div class="col">
-                            <label for="Contrasenya" class="sr-only">Contrasenya</label>
-                            <input type="password" class="form-control" placeholder="Contrasenya">
+                            <label for="usuari" class="sr-only">Username</label>
+                            <?php 
+                            $valueUsuari=(!empty($usuari))?$usuari:'';
+                            $dataUsuari = array(
+                                'name' => 'usuari',
+                                'value' => $valueUsuari,
+                                'class' => 'form-control',
+                                'placeholder' => 'Usuari'
+                            );
+                            echo form_input($dataUsuari);
+                            echo form_error('usuari'); ?>
                         </div>
                     </div>
                 </div>
                 <div class="form-group">
                     <div class="row">
                         <div class="col">
-                            <label for="Contrasenya2" class="sr-only">Contrasenya2</label>
-                            <input type="password" class="form-control" placeholder="Torna a introduir la Contrasenya">
+                            <label for="dni" class="sr-only">DNI</label>
+                            <?php 
+                            $valueDni=(!empty($dni))?$dni:'';
+                            $dataDni = array(
+                                'name' => 'dni',
+                                'value' => $valueDni,
+                                'class' => 'form-control',
+                                'placeholder' => 'DNI'
+                            );
+                            echo form_input($dataNom);
+                            echo form_error('dni'); ?>
+                        </div>
+                        <div class="col">
+                            <label for="naix" class="sr-only">Data de Naixement</label>
+                            <?php 
+                            $valueNaix=(!empty($naix))?$naix:'';
+                            $dataNaix = array(
+                                'name' => 'naix',
+                                'value' => $valueNaix,
+                                'class' => 'form-control',
+                                'type' => 'date'
+                            );
+                            echo form_input($dataNaix);
+                            echo form_error('naix'); ?>
+                        </div>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="row">
+                        <div class="col">
+                            <label for="correu" class="sr-only">E-mail</label>
+                            <?php 
+                            $valueCorreu=(!empty($correu))?$correu:'';
+                            $dataCorreu = array(
+                                'name' => 'correu',
+                                'value' => $valueCorreu,
+                                'class' => 'form-control',
+                                'placeholder' => 'E-mail',
+                                'type' => 'email'
+                            );
+                            echo form_input($dataCorreu);
+                            echo form_error('correu'); ?>
+                        </div>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="row">
+                        <div class="col">
+                            <label for="password" class="sr-only">Contrasenya</label>
+                            <?php 
+                            $valuePass=(!empty($password))?$password:'';
+                            $dataPass = array(
+                                'name' => 'password',
+                                'value' => $valuePass,
+                                'class' => 'form-control',
+                                'placeholder' => 'Contrasenya',
+                                'type' => 'password'
+                            );
+                            echo form_input($dataPass);
+                            echo form_error('password'); ?>
+                        </div>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="row">
+                        <div class="col">
+                            <label for="password2" class="sr-only">Contrasenya2</label>
+                            <?php 
+                            $valuePass2=(!empty($password2))?$password2:'';
+                            $dataPass2 = array(
+                                'name' => 'password2',
+                                'value' => $valuePass2,
+                                'class' => 'form-control',
+                                'type' => 'password',
+                                'placeholder' => 'Torna a introduïr la Contrasenya'
+                            );
+                            echo form_input($dataPass2);
+                            echo form_error('password2'); ?>
                         </div>
                     </div>
                 </div>
@@ -65,46 +154,103 @@
                     <p class="dadesCobrament">Dades de <br> Cobrament</p>
                 </div>
                 <div class="form-group mt-5 pt-5">
-                    <label for="PayPal" class="sr-only">PayPal</label>
-                    <input type="text" class="form-control" placeholder="Compte de PayPal">
+                    <label for="paypal" class="sr-only">PayPal</label>
+                    <?php 
+                    $valuePaypal=(!empty($paypal))?$paypal:'';
+                    $dataPaypal = array(
+                        'name' => 'paypal',
+                        'value' => $valuePaypal,
+                        'class' => 'form-control',
+                        'type' => 'email',
+                        'placeholder' => 'Compte de Paypal'
+                    );
+                    echo form_input($dataPaypal);
+                    echo form_error('paypal'); ?>
                 </div>
                 <div class="form-group">
                     <div class="row">
                         <div class="col">
-                            <label for="Provincia" class="sr-only">Provincia</label>
-                            <input type="text" class="form-control" placeholder="Provincia">
+                            <label for="provincia" class="sr-only">Provincia</label>
+                            <?php 
+                            $valueProvincia=(!empty($provincia))?$provincia:'';
+                            $dataProvincia = array(
+                                'name' => 'provincia',
+                                'value' => $valueProvincia,
+                                'class' => 'form-control',
+                                'placeholder' => 'Provincia'
+                            );
+                            echo form_input($dataProvincia);
+                            echo form_error('provincia'); ?>
                         </div>
                         <div class="col">
-                            <label for="Poblacio" class="sr-only">Poblacio</label>
-                            <input type="text" class="form-control" placeholder="Poblacio"> 
+                            <label for="poblacio" class="sr-only">Poblacio</label>
+                            <?php 
+                            $valuePoblacio=(!empty($poblacio))?$poblacio:'';
+                            $dataPoblacio = array(
+                                'name' => 'poblacio',
+                                'value' => $valuePoblacio,
+                                'class' => 'form-control',
+                                'placeholder' => 'Poblacio'
+                            );
+                            echo form_input($dataPoblacio);
+                            echo form_error('poblacio'); ?>
                         </div>
                         <div class="col">
-                            <label for="CodiPostal" class="sr-only">CodiPostal</label>
-                            <input type="text" class="form-control" placeholder="CodiPostal"> 
+                            <label for="codipostal" class="sr-only">CodiPostal</label>
+                            <?php 
+                            $valueCP=(!empty($codipostal))?$codipostal:'';
+                            $dataCP = array(
+                                'name' => 'codipostal',
+                                'value' => $valueCP,
+                                'class' => 'form-control',
+                                'placeholder' => 'Codi Postal'
+                            );
+                            echo form_input($dataCP);
+                            echo form_error('codipostal'); ?>
                         </div>
                     </div>
                 </div>
                 <div class="form-group">
-                    <label for="Direccio" class="sr-only">Direccio</label>
-                    <input type="text" class="form-control" placeholder="Carrer / Avinguda">
+                    <label for="carrer" class="sr-only">Direccio</label>
+                    <?php 
+                    $valueCarrer=(!empty($carrer))?$carrer:'';
+                    $dataCarrer = array(
+                        'name' => 'carrer',
+                        'value' => $valueCarrer,
+                        'class' => 'form-control',
+                        'placeholder' => 'Carrer / Avinguda'
+                    );
+                    echo form_input($dataCarrer);
+                    echo form_error('carrer'); ?>
                 </div>
                 <div class="form-group">
                     <div class="row">
                         <div class="col">
-                            <label for="NApartament" class="sr-only">NºApartament</label>
-                            <input type="text" class="form-control" placeholder="Numero">
+                            <label for="numero" class="sr-only">NºApartament</label>
+                            <?php 
+                            $valueNum=(!empty($numero))?$numero:'';
+                            $dataNum = array(
+                                'name' => 'numero',
+                                'value' => $valueNum,
+                                'class' => 'form-control',
+                                'placeholder' => 'Numero'
+                            );
+                            echo form_input($dataNum);
+                            echo form_error('numero'); ?>
                         </div>
                         <div class="col">
-                            <label for="Pis" class="sr-only">Pis</label>
-                            <input type="text" class="form-control" placeholder="Pis / Nº Apartament">
+                            <label for="pis" class="sr-only">Pis</label>
+                            <input name="pis" type="text" class="form-control" placeholder="Pis / Nº Apartament">
                         </div>
                     </div>
                 </div>
                 <div class="form-group pt-5 text-center">
                     <label for="enviar" class="sr-only"></label>
-                    <button type="submit" class="custom-btn2 btn-7"><span>REGISTRAR</span></button>
+                    <button name="register" type="submit" class="custom-btn2 btn-7"><span>REGISTRAR</span></button>
                 </div>
-            </form>
+
+            <?php echo form_close(); ?>
+
         </div>
 </div>
 
