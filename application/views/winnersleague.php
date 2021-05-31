02cd13639d13ec430413fc7a8608da044d27b80e
csanchez19 <csanchezf197@gmail.com>
2021-05-31 08:48:01 +0200

    dfghjk

--- application/views/winnersleague.php
+++ application/views/winnersleague.php
@@ -2,23 +2,19 @@
 <link rel="stylesheet" href="<?php echo base_url(); ?>css/winners.css">
 
 <style>
-    body
-    {
-                background-color: #e77304;
-    }
+
 </style>
 
-<div class="container-fluid top_divW">
+<div class="container-fluid top_div">
     <div class="row">
-        <div class="text-center col-12 div_botonesW">
-            <h1 class="text-center div_botones2" style="visibility: hidden">Winners League</h1>
+        <div class="text-center col-12 div_botones">
+            <h1 class="text-center marginado div_botones2">Winners League</h1>
         </div>
     </div>
 </div>
 
-<div class="container-fluid div_orange">
-<div class="container marginado top_Winners">
-    <h2 class="text-center">Benvingut a la <span>Winners League!</span></h2>
+<div class="container-fluid marginado">
+<div class="container marginado">
     <p style="display:none;" class="marginado">Aquest és el Rànking de la <span>Winners League!</span></p>
 </div>
 
